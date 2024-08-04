ca913f2a47031a9b60889a90ab1a7c70f78c5da8
Mackenzie <mackbaird@outlook.com>
2024-08-04 23:38:57 +0100

    CSS/Styling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -25,13 +25,18 @@ export default function Dictionary() {
 
   return (
     <div className="Dictionary">
-      <form onSubmit={search}>
-        <input
-          type="search"
-          autoFocus={true}
-          onChange={handleKeywordChange}
-        ></input>
-      </form>
+      <section>
+        <h1>What word do you want to look up?</h1>
+        <form onSubmit={search}>
+          <input
+            type="search"
+            placeholder="Search for a word"
+            autoFocus={true}
+            onChange={handleKeywordChange}
+          ></input>
+        </form>
+        <div className="hint">Suggested Words: sunset, yoga, forest, etc.</div>
+      </section>
       <Results results={results} />
     </div>
   );
--- src/Meaning.js
+++ src/Meaning.js
@@ -1,17 +1,17 @@
 import React from "react";
 import Synonyms from "./Synonyms";
+import "./Meaning.css";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      <p>
-        {props.meaning.definition}
-        <br />
+      <div className="definition">{props.meaning.definition}</div>
+      <br />
+      <div className="example">
         <em>{props.meaning.example}</em>
-        <Synonyms synonyms={props.meaning.synonyms} />
-      </p>
+      </div>
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
